aff80108415c7b6fb4c03975d3a33575915cfc7f
GrabbitGames <154015851+GrabbitGames@users.noreply.github.com>
2025-04-03 08:05:43 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://focus-bare2.onrender.com/',
+    bare:'https://focus-bare2.onrender.com',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
